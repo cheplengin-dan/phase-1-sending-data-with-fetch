81729675d97ffecb751d0ae0a3af16a810e469bd
Duncan <cheplenginduncan@gmail.com>
2023-04-01 10:33:17 +0300

    almost there

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(userName, userEmail){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+      const newId = data.id;
+      const idElement = document.createElement('p');
+      idElement.innerHTML = newId;
+      document.body.appendChild(idElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement('p');
+      errorElement.innerHTML = error.message;
+      document.body.appendChild(errorElement);
+    });
+};
+
+
+
+
+
+
